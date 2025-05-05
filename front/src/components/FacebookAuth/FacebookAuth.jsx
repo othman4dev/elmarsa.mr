302e098dd658e53a25ee225faa3ad6fbc5df6946
othman4dev <othmandevsup@gmail.com>
2025-05-05 22:03:54 +0100

    Changing the facebook Oauth url

--- front/src/components/FacebookAuth/FacebookAuth.jsx
+++ front/src/components/FacebookAuth/FacebookAuth.jsx
@@ -3,7 +3,7 @@ import FacebookLogin from "react-facebook-login";
 
 const FacebookAuth = () => {
   const responseFacebook = (response) => {
-    window.location.href = "https://localhost:3000//auth/facebook";
+    window.location.href = "https://localhost:3000/api/api/auth/facebook";
   };
 
   return <FacebookLogin appId="1038851094238369" autoLoad={false} fields="name,email,picture" callback={responseFacebook} icon="fa-facebook" />;
